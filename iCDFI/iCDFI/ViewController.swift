89de5bab7c12ed6ec347578a6fc15923cbf316fa
ffranjavier <franciscojavier@Ernestos-Mac-mini.local>
2015-01-19 00:01:22 -0600

    Initial project

---
+++ iCDFI/iCDFI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCDFI
+//
+//  Created by Francisco Javier López Riojas on 1/18/15.
+//  Copyright (c) 2015 Francisco Javier López Riojas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
